04b5fc36a7ccaeb534ec09e94cd48dfd99fa0758
Alex Lopes Pereira <alexlopespereira@gmail.com>
2016-12-27 10:31:01 -0400

    adding db schema

--- db_schema/db_schema.sql
+++ db_schema/db_schema.sql
@@ -13,7 +13,7 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
+-- SET row_security = off;
 
 --
 -- TOC entry 11 (class 2615 OID 32607)
